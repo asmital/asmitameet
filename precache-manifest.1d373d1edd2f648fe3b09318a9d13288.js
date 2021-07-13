abd9ef907d3909284f95d421f5d8f274dd27613e
“asmital” <limaye.asmita@gmail.com>
2021-07-13 18:50:26 +0530

    Updates

---
+++ precache-manifest.1d373d1edd2f648fe3b09318a9d13288.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70851448a03eda9ca209a055e5e5a4a5",
+    "url": "/asmitameet/index.html"
+  },
+  {
+    "revision": "57ce047f957401a84752",
+    "url": "/asmitameet/static/css/2.6788ebe6.chunk.css"
+  },
+  {
+    "revision": "21f283ab9a64e2af306d",
+    "url": "/asmitameet/static/css/main.d17b9232.chunk.css"
+  },
+  {
+    "revision": "57ce047f957401a84752",
+    "url": "/asmitameet/static/js/2.ac887abe.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/asmitameet/static/js/2.ac887abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f283ab9a64e2af306d",
+    "url": "/asmitameet/static/js/main.e626bfcc.chunk.js"
+  },
+  {
+    "revision": "a598a02b89560937ab37",
+    "url": "/asmitameet/static/js/runtime-main.baee6fbb.js"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/asmitameet/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/asmitameet/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/asmitameet/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/asmitameet/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/asmitameet/static/media/fontawesome-webfont.f775f9cc.svg"
+  },
+  {
+    "revision": "580579f4bb59c09672c40eea98481b28",
+    "url": "/asmitameet/static/media/letter_bg_01.580579f4.jpg"
+  },
+  {
+    "revision": "d2cda70b3dfa1fa0adbf699a93a7a686",
+    "url": "/asmitameet/static/media/letter_bg_02.d2cda70b.jpg"
+  },
+  {
+    "revision": "3196ae99c267dd3a5167bcf1b31da1b0",
+    "url": "/asmitameet/static/media/letter_bg_03.3196ae99.jpg"
+  }
+]);
